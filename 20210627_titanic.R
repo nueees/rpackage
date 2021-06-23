4ae2d5cf0f7fbf94a1aa70e38ba4df8fbef51a7a
nueees <nueees@gmail.com>
2021-06-23 12:45:09 +0900

    0623 first Commit

---
+++ 20210627_titanic.R
@@ -0,0 +1,13 @@
+#setwd("C:/Users/Administrator/Documents/R/Kaggle/titanic")
+library(dplyr);library(magrittr);library(caret);library(recipes);
+rm(list=ls())
+read.csv("./data/gender_submission.csv") -> y_test
+bind_rows(read.csv("./data/train.csv") %>%  mutate(index = "train")-> train, 
+          read.csv("./data/test.csv")  %>%  mutate(index = "test") -> x_test ) -> full 
+
+
+str(y_test)
+str(full)
+summary(full)
+
+
